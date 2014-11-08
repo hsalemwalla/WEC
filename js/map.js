fda994f36fa00f9b02bd3667f22ec42e540f8a9f
hsalemwalla <hsalemwalla@gmail.com>
2014-11-07 21:11:58 -0500

    Moved parser code to ui.js

--- js/map.js
+++ js/map.js
@@ -1,32 +1,3 @@
-var mapText;
-var requestText;
-
-function parseFiles() {
-    //var mapInput = document.getElementById("InputMapFile");
-    var mapFile = document.getElementById("InputMapFile").files;
-    var requestFile = document.getElementById("InputRequestFile").files;
-
-    //if (!mapFile || !mapFile[0] || !requestFile || !requestFile[0]) {
-        //// Something isn't right
-        //return;
-    //}
-    
-    var mapReader = new FileReader();
-    var requestReader = new FileReader();
-
-    mapReader.onload = function(e) {
-      var mapText = mapReader.result;
-      console.log(mapText);
-    }
-    requestReader.onload = function(e) {
-      var requestText = requestReader.result;
-      console.log(requestText);
-    }
-
-    mapReader.readAsText(mapFile[0]);
-    requestReader.readAsText(requestFile[0]);
-}
-
 /* 2D array representation of the map
  * To access a given cell, use m[y][x] or m.byXY(x, y)
  * 
---
+++ js/ui.js
@@ -0,0 +1,30 @@
+var mapText;
+var requestText;
+
+function parseFiles() {
+    //var mapInput = document.getElementById("InputMapFile");
+    var mapFile = document.getElementById("InputMapFile").files;
+    var requestFile = document.getElementById("InputRequestFile").files;
+
+    //if (!mapFile || !mapFile[0] || !requestFile || !requestFile[0]) {
+        //// Something isn't right
+        //return;
+    //}
+    
+    var mapReader = new FileReader();
+    var requestReader = new FileReader();
+
+    mapReader.onload = function(e) {
+      var mapText = mapReader.result;
+      console.log(mapText);
+      m_import(mapText);
+    }
+    requestReader.onload = function(e) {
+      var requestText = requestReader.result;
+      console.log(requestText);
+      r_import(requestText);
+    }
+
+    mapReader.readAsText(mapFile[0]);
+    requestReader.readAsText(requestFile[0]);
+}
