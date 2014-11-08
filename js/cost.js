8b4b56f973696e82572ab476b95dce7d5b150cb3
Edison Zhao <edisonzhao4work@gmail.com>
2014-11-07 23:18:46 -0500

    update financial info

--- js/cost.js
+++ js/cost.js
@@ -6,10 +6,27 @@ function revenue(r)
     })
 }
 
-function numOfCars(revenue)
+function profit(input)
 {
-    //For every $400 we are getting, send 1 car
-    return Math.floor(revenue / 400) + 1;
+    profit = revenue(r) - input.length * 50;
+    for (i = 0; i < input.length; i++) {
+        for (j = 0; j < input[i].actions.length; j++) {
+            if (input[i].actions[j].action == 'drive')
+                profit--;
+        }
+        console.log(profit);
+    }
+    return profit;
+}
+
+function waitTime(input)
+{
+    wait_t = 0;
+    for (i = 0; i < input.length; i++) {
+        wait_t += input[i].actions.length - 1;
+        console.log(wait_t);
+    }
+    return wait_t
 }
 
 
